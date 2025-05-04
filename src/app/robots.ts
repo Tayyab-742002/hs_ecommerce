d95e7f54d3afda326cbd4ec10ac4bf80d8e427bb
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2025-05-04 19:31:25 +0500

    update robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,13 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: [
-        "/studio/", // Disallow Sanity Studio
-        "/api/", // Disallow API routes
-        "/admin/", // Disallow admin routes if any
-        "/_next/", // Disallow Next.js system files
-        "/private/", // Disallow private content if any
-      ],
     },
     sitemap: "https://www.hsecommerce.store/api/sitemap.xml",
     host: "https://www.hsecommerce.store",
