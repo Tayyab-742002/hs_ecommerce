671114ea36294eff3fe7a026d79e6dea7a71c02c
Muhammad Tayyab <muhammadtayyab742002@gmail.com>
2025-05-13 18:13:57 +0500

    add umami cloud trackin code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -121,6 +121,11 @@ export default function RootLayout({
         />
         <meta name="msapplication-TileColor" content="#0D3D4C" />
         <meta name="theme-color" content="#0D3D4C" />
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="42a19837-d4a5-428e-8998-7f72dc062dea"
+        ></script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
